0a6d1f95345cba8fb592b9070dd8ed1c0cca8d1d
iobio-zjut <guijunlab06@163.com>
2025-11-04 10:17:51 +0800

    update simulation

---
+++ simulation/run_rosetta/DB5.5/stage_1/run_stage_1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+file_path="$SCRIPT_DIR/lst.txt"
+target_dir="$SCRIPT_DIR"
+list_name=()
+parallel_limit=10 # 设置并行处理的限制，这里是一次运行的目标数
+
+while IFS= read -r line; do
+  list_name+=("$line")
+done < "$file_path"
+
+for name in "${list_name[@]}"; do
+  if [ ! -d "$target_dir/${name}/tbm/trans_mat_5model_bestscore" ]; then
+    echo "Running domain assembly (tbm)...${name}"
+    mkdir $target_dir/${name}/tbm/trans_mat_5model_bestscore
+    cp $target_dir/flags $target_dir/${name}/tbm
+    cp $target_dir/stage1.sh $target_dir/${name}/tbm
+    cd $target_dir/${name}/tbm
+    chmod 755 $target_dir/${name}/tbm/stage1.sh
+    ./stage1.sh &
+    ((i=i+1)) # 计数器，记录已经启动的任务数
+    if [ "$i" -ge "$parallel_limit" ]; then
+      i=0 # 达到并行处理限制后，等待所有任务完成
+      wait
+    fi
+    cd $target_dir
+  fi
+done
+
+# 等待剩余的任务完成
+wait
